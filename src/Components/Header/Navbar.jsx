7b7206351e52599a18c36dbd25b2d62966065fc3
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-05-19 01:55:14 +0600

    Banner Complete

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -13,13 +13,13 @@ const Navbar = () => {
 
     return (
         <div>
-            <div className="navbar p-3 bg-sky-800">
+            <div className="navbar p-3 bg-sky-800 sticky top-0">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="white"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-sky-800 text-white rounded-box w-52">
                             <Link to={"/"}>Home</Link>
                             <Link to={"/alltoys"} tabIndex={0} className="justify-between"> All Toys
                             </Link>
@@ -29,8 +29,8 @@ const Navbar = () => {
                         </ul>
                     </div>
                     <div className="flex items-center">
-                        <img className="h-16 w-16 rounded-full" src="https://w7.pngwing.com/pngs/700/802/png-transparent-hot-wheels-model-car-die-cast-toy-hot-wheels-logo-car-smiley.png" alt="" />
-                        <h1 className="ml-2 text-3xl font-bold text-red-600">Toy <span className="text-yellow-400">Cars</span></h1>
+                        <img className="w-0 h-0 lg:h-16 lg:w-16 rounded-full" src="https://w7.pngwing.com/pngs/700/802/png-transparent-hot-wheels-model-car-die-cast-toy-hot-wheels-logo-car-smiley.png" alt="" />
+                        <h1 className="ml-2 text-2xl lg:text-3xl font-bold text-red-600">Toy <span className="text-yellow-400">Cars</span></h1>
                     </div>
                 </div>
                 <div className="navbar-center hidden lg:flex ">
@@ -45,17 +45,17 @@ const Navbar = () => {
                 </div>
                 <div className="navbar-end">
                     {
-                        user ? <Link className="mr-4 bg-yellow-400 p-3 font-bold text-white rounded-lg hover:bg-red-500"> <button onClick={handleSignOut}>Log out</button></Link>
+                        user ? <Link className="mr-2 lg:mr-4 bg-yellow-400 p-2 lg:p-3 font-bold text-white rounded-lg hover:bg-red-500"> <button onClick={handleSignOut}>Log out</button></Link>
                             :
-                            <Link className="mr-4 bg-yellow-400 p-3 font-bold text-white rounded-lg hover:bg-red-500" to={"/login"}> <button>Login</button></Link>
+                            <Link className="mr-2 lg:mr-4 bg-yellow-400 p-2 lg:p-3 font-bold text-white rounded-lg hover:bg-red-500" to={"/login"}> <button>Login</button></Link>
                     }
 
                     {
                         user ? <div className="tooltip tooltip-bottom" data-tip={user.displayName}>
-                            <img className="h-14 w-14 rounded-full" src={user.photoURL} referrerPolicy="no-referrer" />
+                            <img className="w-10 h-10 lg:h-14 lg:w-14 rounded-full" src={user.photoURL} referrerPolicy="no-referrer" />
                         </div>
                             :
-                            <Link className="mr-4 bg-red-600 p-3 font-bold text-white rounded-lg hover:bg-yellow-400" to={"/signup"}> <button>Sign Up</button></Link>
+                            <Link className="lg:mr-4 bg-red-600 p-2 lg:p-3 font-bold text-white rounded-lg hover:bg-yellow-400" to={"/signup"}> <button>Sign Up</button></Link>
                     }
                 </div>
             </div>
