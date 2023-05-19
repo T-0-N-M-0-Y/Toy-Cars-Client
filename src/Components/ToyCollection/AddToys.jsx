21368d11ceb7e5999390bb912b9bd88a9bea9278
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-05-20 03:32:45 +0600

    Add Toy Data Entry Complete

--- src/Components/ToyCollection/AddToys.jsx
+++ src/Components/ToyCollection/AddToys.jsx
@@ -1,12 +1,38 @@
 
 const AddToys = () => {
 
-    
+    const handleAddaToy = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const toyname = form.toyname.value;
+        const pictureURL = form.pictureURL.value;
+        const email = form.email.value;
+        const subcatagory = form.subcatagory.value;
+        const price = form.price.value;
+        const rating = form.rating.value;
+        const quantity = form.quantity.value;
+        const details = form.details.value;
+
+        const addToy = { name, toyname, pictureURL, email, subcatagory, price, rating, quantity, details }
+
+        console.log(addToy);
+
+        fetch("http://localhost:5000/newtoy", {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(addToy)
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
 
     return (
         <div className="bg-slate-200 p-24">
             <h1 className='text-5xl font-bold text-center mb-5'>Add Your Toys</h1>
-            <form>
+            <form onSubmit={handleAddaToy}>
                 <div className="flex">
                     <div className="form-control lg:w-1/2">
                         <label className="label">
@@ -49,7 +75,7 @@ const AddToys = () => {
                             <span className="label-text">Sub-Catagory</span>
                         </label>
                         <label className="input-group">
-                            <input type="text" name="sub-catagory" placeholder="Sub-Catagory" className="input input-bordered lg:w-full mr-5" />
+                            <input type="text" name="subcatagory" placeholder="Sub-Catagory" className="input input-bordered lg:w-full mr-5" />
                         </label>
                     </div>
                     <div className="form-control lg:w-1/2">
@@ -82,7 +108,7 @@ const AddToys = () => {
                 <textarea className="textarea textarea-bordered lg:w-full my-5" name="details" placeholder="Detail description"></textarea>
 
                 <div className="form-control">
-                    <input className="text-center text-white bg-red-600 p-3 font-bold rounded-lg hover:bg-yellow-500" type="submit" value="Login" />
+                    <input className="text-center text-white bg-red-600 p-3 font-bold rounded-lg hover:bg-yellow-500" type="submit" value="Add" />
                 </div>
             </form>
         </div>
