86b136791264a36024064512676ec8dcb22832c8
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-05-21 23:40:21 +0600

    Final Update

--- src/Components/HomePage/Sponsor.jsx
+++ src/Components/HomePage/Sponsor.jsx
@@ -5,10 +5,21 @@ const Sponsor = () => {
             <h1 className='text-5xl font-bold text-center my-10'>Meet Our Awosome Sponsor</h1>
 
             <div className="grid md:grid-cols-4 grid-cols-2 gap-5">
-                <img className="md:w-96 w-40 md:h-96 h-40" src="https://media.istockphoto.com/id/1288784479/vector/trendy-abstract-vibrant-and-colorful-icon-element-logo.jpg?s=612x612&w=0&k=20&c=tUetN1MR7YhOSwbA-7segIi5kDuy5PNhKVyDYh_X5vI=" alt="" />
-                <img className="md:w-96 w-40 md:h-96 h-40" src="https://lifebeyondsportmedia.com/bestanden/Football-Sponsors/redbull.jpg" alt="" />
-                <img className="md:w-96 w-40 md:h-96 h-40" src="https://e7.pngegg.com/pngimages/519/252/png-clipart-logo-brand-adidas-sponsor-shoe-brands-angle-text.png" alt="" />
-                <img className="md:w-96 w-40 md:h-96 h-40" src="https://www.pngkit.com/png/detail/342-3428792_website-sponsor-republic-of-gamers-logo-png.png" alt="" />
+                <img className="md:w-96 w-40 md:h-96 h-40" src="https://media.istockphoto.com/id/1288784479/vector/trendy-abstract-vibrant-and-colorful-icon-element-logo.jpg?s=612x612&w=0&k=20&c=tUetN1MR7YhOSwbA-7segIi5kDuy5PNhKVyDYh_X5vI=" alt="" data-aos="flip-left"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="2000"/>
+
+                <img className="md:w-96 w-40 md:h-96 h-40" src="https://lifebeyondsportmedia.com/bestanden/Football-Sponsors/redbull.jpg" alt="" data-aos="flip-left"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="2000"/>
+
+                <img className="md:w-96 w-40 md:h-96 h-40" src="https://e7.pngegg.com/pngimages/519/252/png-clipart-logo-brand-adidas-sponsor-shoe-brands-angle-text.png" alt="" data-aos="flip-right"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="2000"/>
+
+                <img className="md:w-96 w-40 md:h-96 h-40" src="https://www.pngkit.com/png/detail/342-3428792_website-sponsor-republic-of-gamers-logo-png.png" alt="" data-aos="flip-right"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="2000"/>
             </div>
 
         </div>
